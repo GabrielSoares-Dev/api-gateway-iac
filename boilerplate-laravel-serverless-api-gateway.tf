f239aa72646c4eb9bf66a391188ab01c086a32db
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-06-04 22:45:29 -0300

    feat: add stage

--- boilerplate-laravel-serverless-api-gateway.tf
+++ boilerplate-laravel-serverless-api-gateway.tf
@@ -16,5 +16,5 @@ resource "aws_apigatewayv2_stage" "boilerplate_laravel_serverless_stage" {
 resource "aws_ssm_parameter" "boilerplate_laravel_serverless_10_ssm_api_gw_id" {
   name  = "/${lower(var.environment)}/api-gw/boilerplate-laravel-10-serverless/id"
   type  = "SecureString"
-  value = aws_apigatewayv2_api.boilerplate_laravel_serverless_api_gw.id
+  value = aws_apigatewayv2_api.boilerplate_laravel_serverless_10_api_gw.id
 }
