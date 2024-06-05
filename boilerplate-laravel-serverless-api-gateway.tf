2fb6f6837e3511a4b7e34df0d704e2fe15df3273
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-06-04 22:30:47 -0300

    feat: add stage

--- boilerplate-laravel-serverless-api-gateway.tf
+++ boilerplate-laravel-serverless-api-gateway.tf
@@ -1,11 +1,11 @@
 resource "aws_apigatewayv2_api" "boilerplate_laravel_serverless" {
-  name          = "boilerplate-laravel-serverless"
+  name          = "${lower(var.environment)}-boilerplate-laravel-serverless"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_stage" "boilerplate_laravel_serverless_stage" {
   api_id      = aws_apigatewayv2_api.boilerplate_laravel_serverless.id
-  name        = lower(var.environment)
+  name        = "$default"
   auto_deploy = true
 
   tags = {
