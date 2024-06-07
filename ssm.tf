9a826241633ff206fce42d52d5d59354eca24808
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-06-07 15:45:36 -0300

    feat: add ssms

--- ssm.tf
+++ ssm.tf
@@ -3,3 +3,15 @@ resource "aws_ssm_parameter" "boilerplate_laravel_serverless_10_ssm_api_gw_id" {
   type  = "SecureString"
   value = aws_apigatewayv2_api.boilerplate_laravel_serverless_10_api_gw.id
 }
+
+resource "aws_ssm_parameter" "boilerplate_laravel_serverless_10_ssm_api_gw_domain_name" {
+  name  = "/${lower(var.environment)}/api-gw/boilerplate-laravel-10-serverless/domain/name"
+  type  = "String"
+  value = aws_apigatewayv2_domain_name.boilerplate_laravel_serverless_10_api_gw_domain.domain_name_configuration[0].target_domain_name
+}
+
+resource "aws_ssm_parameter" "boilerplate_laravel_serverless_10_ssm_api_gw_domain_zone_id" {
+  name  = "/${lower(var.environment)}/api-gw/boilerplate-laravel-10-serverless/domain/zone-id"
+  type  = "String"
+  value = aws_apigatewayv2_domain_name.boilerplate_laravel_serverless_10_api_gw_domain.domain_name_configuration[0].hosted_zone_id
+}
